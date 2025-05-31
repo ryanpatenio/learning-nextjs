0c7bb0cab46854f8c2bff3930154170503d739c3
ryanpatenio <ryanpatenio06@gmail.com>
2025-06-01 00:36:59 +0800

    add package zod & rules lib & registerFormSchema

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,13 +1,21 @@
 "use server"
+
+import { registerFormSchema } from "@/lib/rules";
+
 export async function register(state, formData){
 
-    await new Promise ((resolve) => setTimeout(resolve,3000));
+    //await new Promise ((resolve) => setTimeout(resolve,3000));
+    const validatedFields = registerFormSchema.safeParse({
+        email : formData.get('email'),
+        password : formData.get('password'),
+        confirmPassword : formData.get('confirmPassword')
+    });
 
-   const email = formData.get('email');
-   const password = formData.get('password');
-   const confirmPassword = formData.get('confirmPassword');
-
-   console.log(email)
-   console.log(password)
-   console.log(confirmPassword)
+    if(!validatedFields.success){
+        return {
+            errors : validatedFields.error.flatten().fieldErrors,
+            email : formData.get('email'),
+        }
+    }
+    console.log(validatedFields)
 }
